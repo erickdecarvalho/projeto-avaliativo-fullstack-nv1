216c586d3a6d7f0635274c27c731199928b0f8e3
alex cesar <thealexcesar@gmail.com>
2024-08-24 17:11:10 -0300

    feature/customer: validations + Add CPF unique

--- BACKEND/car_rent/src/main/java/car_rent/api/dtos/CustomerDto.java
+++ BACKEND/car_rent/src/main/java/car_rent/api/dtos/CustomerDto.java
@@ -1,4 +1,4 @@
 package car_rent.api.dtos;
 
-public record CustomerDto(String name, String phone, String email) {
+public record CustomerDto(String name, String phone, String email, String cpf) {
 }
